51cf184be59c7cb10bc9b2a07f8d1c93a4698f92
Leovihildo <Johnsonp.joshua@yahoo.com>
2024-10-18 15:51:19 +0100

    update js

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ function submitIncident(type, description, image) {
 
 // Function to fetch incidents
 function fetchIncidents() {
-    const url = 'http://ontym.infinityfreeapp.com/api/get_recent_posts/'; // Your API endpoint to get posts
+    const url = 'http://ontym.infinityfreeapp.com/wp-json/wp/v2/posts'; // Your API endpoint to get posts
 
     fetch(url)
         .then(response => {
